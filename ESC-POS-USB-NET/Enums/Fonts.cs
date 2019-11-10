862c8ee2847cb729919f16448aeaa63c27dcc940
MTM Suhail <getmtm@gmail.com>
2019-11-10 13:22:10 +0530

    Create Fonts.cs

---
+++ ESC-POS-USB-NET/Enums/Fonts.cs
@@ -0,0 +1,18 @@
+﻿namespace ESC_POS_USB_NET.Enums
+{
+   ///<Summary>
+   /// This enum is used to set font type
+   /// </Summary>
+
+    public enum Fonts
+    {
+        FontA,
+        FontB,
+        FontC,
+        FontD,
+        FontE,
+        SpecialFontA,
+        SpecialFontB
+    }
+}
+
