78b81ea5f45487cf0effc3c9f5f23d4f0fce130a
MTM Suhail <getmtm@gmail.com>
2019-11-10 13:29:48 +0530

    Create IPrintCommand.cs

---
+++ ESC-POS-USB-NET/Interfaces/Command/IPrintCommand.cs
@@ -0,0 +1,21 @@
+﻿namespace ESC_POS_USB_NET.Interfaces.Command
+{
+    internal interface IPrintCommand
+    {
+        int ColsNomal { get; }
+        int ColsCondensed { get; }
+        int ColsExpanded { get; }
+        IFontMode FontMode { get; set; }
+        IFontWidth FontWidth { get; set; }
+        IAlignment Alignment { get; set; }
+        IPaperCut PaperCut { get; set; }
+        IDrawer Drawer { get; set; }
+        IQrCode QrCode { get; set; }
+        IBarCode BarCode { get; set; }
+        IImage Image { get; set; }
+        IInitializePrint InitializePrint { get; set; }
+        byte[] Separator();
+        byte[] AutoTest();
+    }
+}
+
