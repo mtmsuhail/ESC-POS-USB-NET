d516245ced1776560b93a4a6f310a761c629f947
MTM Suhail <getmtm@gmail.com>
2019-11-10 13:29:50 +0530

    Create IQrCode.cs

---
+++ ESC-POS-USB-NET/Interfaces/Command/IQrCode.cs
@@ -0,0 +1,11 @@
+﻿using ESC_POS_USB_NET.Enums;
+
+namespace ESC_POS_USB_NET.Interfaces.Command
+{
+    internal interface IQrCode
+    {
+        byte[] Print(string qrData);
+        byte[] Print(string qrData, QrCodeSize qrCodeSize);
+    }
+}
+
