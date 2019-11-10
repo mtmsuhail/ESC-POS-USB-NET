470e20dd714695be9e79e211790865777c044898
MTM Suhail <getmtm@gmail.com>
2019-11-10 15:38:51 +0530

    Create MainWindow.xaml.cs

---
+++ ESC-POS-USB-NET-DEMO/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using ESC_POS_USB_NET.Printer;
+using System;
+using System.Drawing;
+using System.Windows;
+
+namespace ESC_POS_USB_NET_DEMO
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Printer printer = new Printer(txtPrinterName.Text);
+            printer.TestPrinter();
+            printer.FullPaperCut();
+            printer.PrintDocument();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Printer printer = new Printer(txtPrinterName.Text);
+            Bitmap image =new Bitmap ( Bitmap.FromFile("Icon.bmp"));
+            printer.Image(image);
+            printer.FullPaperCut();
+            printer.PrintDocument();
+        }
+    }
+}
