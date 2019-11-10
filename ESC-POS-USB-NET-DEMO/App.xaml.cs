82f9bfa28e993f69d25b3a0b9544b971464bcbde
MTM Suhail <getmtm@gmail.com>
2019-11-10 15:39:08 +0530

    Create App.xaml.cs

---
+++ ESC-POS-USB-NET-DEMO/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ESC_POS_USB_NET_DEMO
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
