e56d021fcabbe3ac985ac9e9a90785cdf1e20f2f
MTM Suhail <getmtm@gmail.com>
2019-11-10 13:29:31 +0530

    Create IBarCode.cs

---
+++ ESC-POS-USB-NET/Interfaces/Command/IBarCode.cs
@@ -0,0 +1,10 @@
+﻿namespace ESC_POS_USB_NET.Interfaces.Command
+{
+    interface IBarCode
+    {
+        byte[] Code128(string code);
+        byte[] Code39(string code);
+        byte[] Ean13(string code);
+    }
+}
+
