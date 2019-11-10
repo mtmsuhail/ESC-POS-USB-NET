51c8978e734193905c2b00f94d919fcf682b2e75
MTM Suhail <getmtm@gmail.com>
2019-11-10 13:30:23 +0530

    Create FontWidth.cs

---
+++ ESC-POS-USB-NET/Epson Commands/FontWidth.cs
@@ -0,0 +1,24 @@
+﻿using ESC_POS_USB_NET.Extensions;
+using ESC_POS_USB_NET.Interfaces.Command;
+
+namespace ESC_POS_USB_NET.EpsonCommands
+{
+    public class FontWidth : IFontWidth
+    {
+        public byte[] Normal()
+        {
+            return new byte[] { 27, '!'.ToByte(), 0 };
+        }
+
+        public byte[] DoubleWidth2()
+        {
+            return new byte[] { 29, '!'.ToByte(), 16 };
+        }
+
+        public byte[] DoubleWidth3()
+        {
+            return new byte[] { 29, '!'.ToByte(), 32 };
+        }
+    }
+}
+
