aad0b132426a18dc9f3e7281e187998d0f27f261
MTM Suhail <getmtm@gmail.com>
2019-11-10 13:30:09 +0530

    Create Alignment.cs

---
+++ ESC-POS-USB-NET/Epson Commands/Alignment.cs
@@ -0,0 +1,25 @@
+﻿using ESC_POS_USB_NET.Extensions;
+using ESC_POS_USB_NET.Interfaces.Command;
+
+namespace ESC_POS_USB_NET.EpsonCommands
+{
+    public class Alignment : IAlignment
+
+    {
+        public byte[] Left()
+        {
+            return new byte[] { 27, 'a'.ToByte(), 0 };
+        }
+
+        public byte[] Right()
+        {
+            return new byte[] { 27, 'a'.ToByte(), 2 };
+        }
+
+        public byte[] Center()
+        {
+            return new byte[] { 27, 'a'.ToByte(), 1 };
+        }
+    }
+}
+
