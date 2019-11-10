11726c49f210cfa977973b2f28dd0fdb332c0051
MTM Suhail <getmtm@gmail.com>
2019-11-10 13:30:15 +0530

    Create Drawer.cs

---
+++ ESC-POS-USB-NET/Epson Commands/Drawer.cs
@@ -0,0 +1,13 @@
+﻿using ESC_POS_USB_NET.Interfaces.Command;
+
+namespace ESC_POS_USB_NET.EpsonCommands
+{
+    public class Drawer : IDrawer
+    {
+        public byte[] Open()
+        {
+            return new byte[] { 27, 112, 0, 60, 120 };
+        }
+    }
+}
+
