4f0bd3d2b1c6171be401ed672bc3c42c5b01e5f7
MTM Suhail <getmtm@gmail.com>
2019-11-10 13:30:03 +0530

    Create InitializePrint.cs

---
+++ ESC-POS-USB-NET/Epson Commands/InitializePrint.cs
@@ -0,0 +1,14 @@
+﻿using ESC_POS_USB_NET.Extensions;
+using ESC_POS_USB_NET.Interfaces.Command;
+
+namespace ESC_POS_USB_NET.EpsonCommands
+{
+    public class InitializePrint : IInitializePrint
+    {
+        public byte[] Initialize()
+        {
+            return new byte[] { 27, '@'.ToByte() };
+        }
+    }
+}
+
