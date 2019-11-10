844942d3bf2bb8064e1b444f42f2ac16d4f28ea3
MTM Suhail <getmtm@gmail.com>
2019-11-10 13:29:36 +0530

    Create IFontMode.cs

---
+++ ESC-POS-USB-NET/Interfaces/Command/IFontMode.cs
@@ -0,0 +1,19 @@
+﻿using ESC_POS_USB_NET.Enums;
+
+namespace ESC_POS_USB_NET.Interfaces.Command
+{
+    internal interface IFontMode
+    {
+        byte[] Bold(string value);
+        byte[] Bold(PrinterModeState state);
+        byte[] Underline(string value);
+        byte[] Underline(PrinterModeState state);
+        byte[] Expanded(string value);
+        byte[] Expanded(PrinterModeState state);
+        byte[] Condensed(string value);
+        byte[] Condensed(PrinterModeState state);
+        byte[] Font(string value, Fonts state);
+        byte[] Font(Fonts state);
+    }
+}
+
