fd9a10f0c2309b76e15fcb888a47476a7acaf069
MTM Suhail <getmtm@gmail.com>
2019-11-10 13:29:53 +0530

    Create IPrinter.cs

---
+++ ESC-POS-USB-NET/Interfaces/Printer/IPrinter.cs
@@ -0,0 +1,47 @@
+﻿using ESC_POS_USB_NET.Enums;
+
+namespace ESC_POS_USB_NET.Interfaces.Printer
+{
+    internal interface IPrinter
+    {
+        int ColsNomal { get; }
+        int ColsCondensed { get; }
+        int ColsExpanded { get; }
+        void PrintDocument();
+        void Append(string value);
+        void Append(byte[] value);
+        void AppendWithoutLf(string value);
+        void NewLine();
+        void NewLines(int lines);
+        void Clear();
+        void Separator();
+        void AutoTest();
+        void TestPrinter();
+        void Font(string value, Fonts state);
+        void BoldMode(string value);
+        void BoldMode(PrinterModeState state);
+        void UnderlineMode(string value);
+        void UnderlineMode(PrinterModeState state);
+        void ExpandedMode(string value);
+        void ExpandedMode(PrinterModeState state);
+        void CondensedMode(string value);
+        void CondensedMode(PrinterModeState state);
+        void NormalWidth();
+        void DoubleWidth2();
+        void DoubleWidth3();
+        void AlignLeft();
+        void AlignRight();
+        void AlignCenter();
+        void FullPaperCut();
+        void PartialPaperCut();
+        void OpenDrawer();
+        void Image(string path);
+        void QrCode(string qrData);
+        void QrCode(string qrData, QrCodeSize qrCodeSize);
+        void Code128(string code);
+        void Code39(string code);
+        void Ean13(string code);
+        void InitializePrint();
+    }
+}
+
