aa4df9483ab1eec62b3d0c062f1ed0828b4faecb
MTM Suhail <getmtm@gmail.com>
2019-11-10 13:29:38 +0530

    Create IFontWidth.cs

---
+++ ESC-POS-USB-NET/Interfaces/Command/IFontWidth.cs
@@ -0,0 +1,10 @@
+﻿namespace ESC_POS_USB_NET.Interfaces.Command
+{
+    internal interface IFontWidth
+    {
+        byte[] Normal();
+        byte[] DoubleWidth2();
+        byte[] DoubleWidth3();
+    }
+}
+
